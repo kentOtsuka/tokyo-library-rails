51601cd30fe74dff1c9fbe51989251459f15b662
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-15 23:14:17 +0900

    add：RackCorsを導入

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+		# 開発環境では以下の通りでOKだが、本番環境では実際のURLに書き換える
+		# localhost:8080：Vue.jsの開発環境
+    origins 'localhost:8080'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
