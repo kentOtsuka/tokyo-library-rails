98ddf48e49e732dcf21a6b38884da197d4cd18b5
isakashiori <isslamdunk0000@gmail.com>
2022-08-25 19:29:23 +0900

    fix:libraries_service.rbにバリデーション追加

--- app/models/libraries_service.rb
+++ app/models/libraries_service.rb
@@ -2,5 +2,5 @@ class LibrariesService < ApplicationRecord
   belongs_to :library
   belongs_to :service
 
-  validates :library_id, uniqueness: { scope: :service_id}
+  validates :library_id, uniqueness: { scope: :service_id }
 end
