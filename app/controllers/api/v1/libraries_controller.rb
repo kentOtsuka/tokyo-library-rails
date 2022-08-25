6033931a80740f90385af3d4e20b1eee7e6f449b
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-25 19:55:52 +0900

    add：Api::v1::libraries_controllerを作成_2

--- app/controllers/api/v1/libraries_controller.rb
+++ app/controllers/api/v1/libraries_controller.rb
@@ -9,6 +9,9 @@ class Api::V1::LibrariesController < ApplicationController
         open: l.open_time.strftime("%-H:%M"),
         close: l.close_time.strftime("%-H:%M"),
         seats: l.seats,
+        opening_details: l.opening_details,
+        close_date_first: l.closed_body_first,
+        close_date_second: l.closed_body_second,
         position: {
           lat: l.lat.to_f,
           lng: l.lng.to_f
