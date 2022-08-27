825e6cc685dbba4115e242b3b0b388569dc768df
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-27 23:11:40 +0900

    fix：フロント側との接続を変更_3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'tokyo-library-app.web.app'
-    # origins 'localhost:8080'
+    origins Rails.application.credentials.api[:api_domain] || 'localhost:8080'
 
     resource '*',
       headers: :any,
