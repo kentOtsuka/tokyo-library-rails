e7972bac405a095227047259339f45bc12e4d4bf
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-27 14:37:40 +0900

    add：自治体、サービスのApiコントローラを作成

---
+++ app/controllers/api/v1/administrations_controller.rb
@@ -0,0 +1,58 @@
+class Api::V1::AdministrationsController < ApplicationController
+  def index
+    administrations = Administration.all
+    response = []
+    administrations.each do |l|
+      response << {
+        id: l.id,
+        name: l.name,
+      }
+    end
+    render json: { administrations: response }
+  end
+
+  def show
+    response = []
+    # 自治体の複数検索
+    if params[:ids]
+      # サービスも踏まえた詳細検索
+      if params[:serviceId]
+        service = Service.find(params[:serviceId])
+        libraries = service.libraries.where(administration_id:  params[:ids].split(',').map(&:to_i)).includes(:administration)
+      else
+        libraries = Library.where(administration_id:  params[:ids].split(',').map(&:to_i)).includes(:administration, :services)
+      end
+    else
+      # サービスも踏まえた詳細検索
+      if params[:serviceId]
+        service = Service.find(params[:serviceId])
+        libraries = service.libraries.where(administration_id: params[:id])
+      else
+        administration = Administration.find(params[:id])
+        libraries = administration.libraries.includes(:services)
+      end
+    end
+    libraries.each do |l|
+      response << {
+        title: l.name,
+        address: l.administration.name + l.address,
+        open: l.open_time.strftime("%-H:%M"),
+        close: l.close_time.strftime("%-H:%M"),
+        seats: l.seats,
+        opening_details: l.opening_details,
+        close_date_first: l.closed_body_first,
+        close_date_second: l.closed_body_second,
+        services: l.services,
+        position: {
+          lat: l.lat.to_f,
+          lng: l.lng.to_f
+        },
+        pinicon: {
+          url: 'http://maps.google.co.jp/mapfiles/ms/icons/green-dot.png',
+          scaledSize: { width: 40, height: 40, f: 'px', b: 'px' },
+        },
+      }
+    end
+    render json: { libraries: response }
+  end
+end
